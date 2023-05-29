e1044c4bde891d31d755a1ea4e5f53a2ca7f8961
dannykoy <126118436+dannykoy@users.noreply.github.com>
2023-05-29 19:23:26 -0400

    Professional Modification

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const choice = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-
-const randomIndex = Math.floor(Math.random() * choice.length);
-
-const randomChoice = choice[randomIndex];
-
-return randomChoice;
-
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  const randomChoice = choices[randomIndex];
+  return randomChoice;
 }
-const randomChoice = getComputerChoice();
-console.log(randomChoice);
+
+let playerSelection = prompt("Choose: ");
+playerSelection = playerSelection.toLowerCase();
+
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else {
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
