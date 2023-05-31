c58680be11f5f5e81c625644af23f4979d8b6761
dannykoy <126118436+dannykoy@users.noreply.github.com>
2023-05-31 13:06:22 -0400

    Created loop function for a game of 5 rounds.

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function getComputerChoice() {
   return randomChoice;
 }
 
-let playerSelection = prompt("Choose: ");
-playerSelection = playerSelection.toLowerCase();
+// let playerSelection = prompt("Choose: ");
+// playerSelection = playerSelection.toLowerCase();
 
-const computerSelection = getComputerChoice();
+// const computerSelection = getComputerChoice();
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
@@ -25,4 +25,41 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playRound(playerSelection, computerSelection));
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      let playerSelection = prompt("Choose: ");
+      playerSelection = playerSelection.toLowerCase();
+  
+      const computerSelection = getComputerChoice();
+  
+      // Call the playRound function to play a single round
+      const roundResult = playRound(playerSelection, computerSelection);
+  
+      // Update scores based on the round result
+      if (roundResult.includes("win")) {
+        playerScore++;
+      } else if (roundResult.includes("lose")) {
+        computerScore++;
+      }
+  
+      // Display the round result
+      console.log("Round " + round + ": " + roundResult);
+    }
+  
+    // Determine the overall winner based on scores
+    if (playerScore > computerScore) {
+      console.log("You win the game!");
+    } else if (playerScore < computerScore) {
+      console.log("You lose the game!");
+    } else {
+      console.log("It's a tie game!");
+    }
+  }
+  
+  // Call the game function to start playing
+  game();
